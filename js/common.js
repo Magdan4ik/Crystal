801d53f0fe60d900e7fbbed5d12d5d4e39aef95e
iMac <imac@iMac-iMac-2.local>
2019-01-14 09:15:50 +0200

    "Pizdorez sluchilsa"

--- js/common.js
+++ js/common.js
@@ -7,4 +7,43 @@ window.addEventListener('load', function () {
 
   // $("input[type='tel']").mask("+38 (099) 999 99 99");
 
+        // $(function () {
+        //   $.scrollify({
+        //     section: ".scroll-point",
+        //   });
+        // });
+
+  new WOW().init();
+
+  // const slider = jQuery('.home');
+  // slider.slick();
+  // slider.on('wheel', (function (e) {
+  //   e.preventDefault();
+
+  //   if (e.originalEvent.deltaY < 0) {
+  //     $(this).slick('slickNext');
+  //   } else {
+  //     $(this).slick('slickPrev');
+  //   }
+  // }));
+
+
+
+  // const $pageslider = $(".home").bxSlider({
+  //   pager: true,
+  //   mode: 'fade',
+  //   speed: 800,
+  //   auto: false,
+  //   infiniteLoop: false,
+  //   controls: false,
+  //   preventDefaultSwipeX: false,
+
+  // });
+
+  // $(document).mousewheel( (event, delta) => {
+  //   delta < 0 ? $pageslider.goToNextSlide() : $pageslider.goToPrevSlide();
+  // });
+  
+
+
 });
