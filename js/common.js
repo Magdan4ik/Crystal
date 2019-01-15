347d5b8bc6a1f93654a69b252dc6d6f6328fcf1b
iMac <imac@iMac-iMac-2.local>
2019-01-15 15:07:23 +0200

    опыты над scrollmagic

--- js/common.js
+++ js/common.js
@@ -1,106 +1,45 @@
 "use strict";
-window.addEventListener('DOMContentLoaded', function () {
-
-});
+window.addEventListener('DOMContentLoaded',
+  function () {
+    // stare bzdo.. nechipaite
+  }
+);
 
 window.addEventListener('load', function () {
 
 
 
-    // var ctrl = new ScrollMagic.Controller({
-    //   globalSceneOptions: {
-    //     triggerHook: 'onLeave'
-    //   }
-    // });
-
-    // // Create scene
-    // $("section").each(function () {
-
-    //   var name = $(this).attr('id');
-
-    //   new ScrollMagic.Scene({
-    //       triggerElement: this,
-    //       // duration: 10000
-    //       // triggerHook: 0    // 0.7 // 1
-    //     })
-    //     .setPin(this)
-    //     .setClassToggle(this, "myclass")
-    //     .addIndicators({
-    //       colorStart: "rgba(255,255,255,0.5)",
-    //       colorEnd: "rgba(255,255,255,0.5)",
-    //       colorTrigger: "rgba(255,255,255,1)",
-    //       name: name
-    //     })
-
-    //     .loglevel(3)
-    //     .addTo(ctrl);
-
-    // });
+    var ctrl = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave'
+      }
+    });
 
 
+    document.querySelectorAll('.home section').forEach(section => {
+
+      var name = section.id;
+
+      new ScrollMagic.Scene({
+          triggerElement: section
+          // duration: 10000   // поставь это и будеш крутить скрол оч долго
+          // triggerHook: 0    // 0.7 // 1    // a eta huita => 0 - top okna; 1 - dno okna
+        })
+        .setPin(section)
+        .setClassToggle(section, "myclass") 
+        .addIndicators({  // индикатор это подсказка справа
+          colorStart: "rgba(255,255,255,0.5)",
+          colorEnd: "rgba(255,255,255,0.5)",
+          colorTrigger: "rgba(255,255,255,1)",
+          name: name
+        })
+
+        .loglevel(3)
+        .addTo(ctrl);
+
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // $("input[type='tel']").mask("+38 (099) 999 99 99");
-
-        // $(function () {
-        //   $.scrollify({
-        //     section: ".scroll-point",
-        //   });
-        // });
-
-  // new WOW().init();
-
-  // const slider = jQuery('.home');
-  // slider.slick();
-  // slider.on('wheel', (function (e) {
-  //   e.preventDefault();
-
-  //   if (e.originalEvent.deltaY < 0) {
-  //     $(this).slick('slickNext');
-  //   } else {
-  //     $(this).slick('slickPrev');
-  //   }
-  // }));
-
-
-
-  // const $pageslider = $(".home").bxSlider({
-  //   pager: true,
-  //   mode: 'fade',
-  //   speed: 800,
-  //   auto: false,
-  //   infiniteLoop: false,
-  //   controls: false,
-  //   preventDefaultSwipeX: false,
-
-  // });
-
-  // $(document).mousewheel( (event, delta) => {
-  //   delta < 0 ? $pageslider.goToNextSlide() : $pageslider.goToPrevSlide();
-  // });
-
-
-  
 
 
 });
