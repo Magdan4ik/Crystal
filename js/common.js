aecc866ec95c7a51c240589c5cfa6a60bc0985a5
Vladislav <vladd0193@gmail.com>
2019-02-07 13:15:39 +0200

    Home page

--- js/common.js
+++ js/common.js
@@ -5,41 +5,59 @@ window.addEventListener('DOMContentLoaded',
   }
 );
 
+
+
+
+
 window.addEventListener('load', function () {
 
+    const d = document;
+    const w = window;
 
 
-    var ctrl = new ScrollMagic.Controller({
-      globalSceneOptions: {
-        triggerHook: 'onLeave'
-      }
-    });
+     /* OnePage Scroll */
+    (function(){
+      const video = d.getElementById('h-video');
+      onePageScroll(".home", {
+        pagination: false,
+        animationTime: 1500,
+        footer: "footer",
+        delay: 500,
+        afterMove: index => (index == 4)? video.play() : video.pause()
+      });
+    }());
 
+    /* Brands init slider */
+    (function(){
+      new Glide('.glide', {
+        type: 'carousel',
+        focusAt: 'center',
+        perView: 3
+      }).mount()
+    }());
 
-    document.querySelectorAll('.home section').forEach(section => {
-
-      var name = section.id;
-
-      new ScrollMagic.Scene({
-          triggerElement: section
-          // duration: 10000   // поставь это и будеш крутить скрол оч долго
-          // triggerHook: 0    // 0.7 // 1    // a eta huita => 0 - top okna; 1 - dno okna
-        })
-        .setPin(section)
-        .setClassToggle(section, "myclass") 
-        .addIndicators({  // индикатор это подсказка справа
-          colorStart: "rgba(255,255,255,0.5)",
-          colorEnd: "rgba(255,255,255,0.5)",
-          colorTrigger: "rgba(255,255,255,1)",
-          name: name
-        })
-
-        .loglevel(3)
-        .addTo(ctrl);
-
-    });
-
-
-
+    /*Insta */
+    (function(){
+      fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=1820676164.1677ed0.87c8ed8be5fc4beab54d7a7b7d27633d&count=6')
+      .then(res => res.json())
+      .then(res => {
+          const ul = d.getElementById('instafeed');
+          if(ul) {
+            res.data.forEach(el => {
+                let img     = d.createElement('img');
+                let li      = d.createElement('li');
+                let link    = d.createElement('a');
+                    li.className = "instafeed__item";
+                    img.src = el.images.low_resolution.url;
+                    link.href = el.link;
+                    link.target = '_blank';
+                    link.appendChild(img);
+                    li.appendChild(link);
+                    ul.appendChild(li);
+            });
+          }
+      })
+      .catch(err => console.error('Insta Error:', err));
+    }());
 
 });
